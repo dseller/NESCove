32b330652e1169f1ec9b3d39c222fbcd8a83d858
Josh Lloyd <j.nevercast@gmail.com>
2014-04-30 15:58:18 +1200

    Fix opcode mismatch. thealmightyguru.com is wrong

--- NESCove.MOS6502/Opcodes/LDX.cs
+++ NESCove.MOS6502/Opcodes/LDX.cs
@@ -1,4 +1,5 @@
-﻿using NESCove.MOS6502.Addressing;
+﻿using NESCove.Core;
+using NESCove.MOS6502.Addressing;
 
 namespace NESCove.MOS6502.Opcodes
 {
@@ -18,12 +19,17 @@ namespace NESCove.MOS6502.Opcodes
             cpu.RegX = operand;
             switch (cpu.Opcode)
             {
-                case 0xA1: return 2;
+                // There is an error on 
+                // http://www.thealmightyguru.com/Games/Hacking/Wiki/index.php?title=LDX
+                // Immediate opcode is 0xA2, not 0xA1. Thanks unit tests! :D
+                case 0xA2: return 2;
                 case 0xA6: return 3;
                 case 0xB6: return 4;
                 case 0xAE: return 4;
                 case 0xBE: return 4 + CalculateExtraCycles(-1 + cpu.ProgramCounter - AddressingType.ParameterSize.Value, parameter);
             }
+
+            throw new OpcodeExecutionException();
         }
     }
 }
